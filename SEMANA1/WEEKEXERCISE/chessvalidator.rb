37862620be0a09692aa7315929c15f7ba52f3529
Fernando López-Canti <flopezcanti@gmail.com>
2015-11-06 10:00:51 +0100

    segunda semana

--- SEMANA1/WEEKEXERCISE/chessvalidator.rb
+++ SEMANA1/WEEKEXERCISE/chessvalidator.rb
@@ -1,18 +1,49 @@
 require 'PRY'
 
 class Board
+	def initialize grids
+		@grids = grids
+	end
+
+	def LoadBoard
+		grids = IO.read("board.txt").split(" ")
+
+	end
 
 end
 
 class ChessPieze
-	
+	def initialize color, origin
+		@color = [white, black]
+		@origin = [0,0]
+		@movements = (IO.read("movements.txt")
+	end
+
+
+	def checkColor color
+		@color.each do |color|
+
+		end
+	end
+
+
+
+	def CheckMovement board, origin, destination
+		positions = IO.read("movements.txt")
+	end
+
 end
 
 
-class Rock < Pieze
+class Roock < ChessPieze
+	super movements
+	def initialize
+	end
 
 end
 
-class 
+board = Board.new(0)
+
+board.LoadBoard
 
 binding.pry
